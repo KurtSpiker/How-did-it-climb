808e926eb405baddff0b0b6f44479a6a18ef4680
KurtSpiker <kurt.spiker@gmail.com>
2023-05-04 21:08:28 -0600

    state managment on section selection

--- frontend/src/components/Creation.jsx
+++ frontend/src/components/Creation.jsx
@@ -2,17 +2,25 @@ import { Typography } from "@mui/material";
 import ImageMapper from 'react-img-mapper';
 import bolderLayout from '../bolderLayout.png'
 import bolderMap from '../bolderMap.json'
+import { useState } from "react";
 
 export default function Creation() {
+  const [selectedSection, setSelectedSection] = useState(0)
+
   const MAP = {
     name: 'my-map',
     areas: bolderMap,
   };
   return (
     <div>
-      <ImageMapper src={bolderLayout} map={MAP} responsive={true} parentWidth={window.innerWidth}/>
+      <ImageMapper src={bolderLayout} 
+      map={MAP} 
+      responsive={true} 
+      parentWidth={window.innerWidth}
+      stayHighlighted={true}
+      onClick={area => setSelectedSection(area.id)}/>
       <Typography variant="h6">
-        Creating a climb
+        Creating a climb {selectedSection}
       </Typography>
     </div>
     
